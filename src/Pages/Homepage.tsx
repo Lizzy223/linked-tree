789bdfc03aa7441932fc6ed388e9a587c62a704c
Dev-Waterways <113593306+Dev-Waterways@users.noreply.github.com>
2022-10-25 14:08:12 +0100

    made it mobile responsive

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -6,7 +6,7 @@ const Homepage = () => {
   return (
     <Box w='100%' h='100%' display='flex' p={['1rem','4rem']} mt={['1.5rem','0']} justifyItems='center' alignItems='center' flexDirection='column'>
         <Box >
-        <Image id='profile__img’' src='assets/img.png' w='80px' h='80px' alt='' />
+        <Image id='profile__img’' _hover={{src:'hover.png', cursor:'pointer'}} src='assets/img.png' w='80px' h='80px' alt='' />
         </Box>
         <Box position='absolute' top={['10px','40px']} right={['40px','278px']}>
         <Image src='assets/share.png'  _hover={{cursor:'pointer'}} w='40px' display={['none','block']} h='40px' alt=''/>
@@ -38,14 +38,14 @@ const Homepage = () => {
             <Button  as='a' id='book__design' href='https://books.zuri.team/design-rules' target='_blank' w={['100%','90%']} h='4.75rem' m='auto' bg='#EAECF0' borderRadius='8px' color='#101828' fontWeight='500' fontSize='18px' border='1px solid #EAECF0'>
                 Design books
             </Button>
-            <Box display='flex' padding='24px, 32px, 24px, 32px' mt='1rem' gap='6'>
+            <Box display={['none','flex']} padding='24px, 32px, 24px, 32px' mt={['0rem','1rem']} gap='6'>
                 <Image src='assets/slack.png' alt=''/>
                 <Image alt='' src='assets/Icon.png'/>
             </Box>
         </Box>
-        <Box w='90%' mt='6rem' alignItems='center' display='flex' flexDirection='column' >
+        <Box w='90%' mt='6rem' alignItems='center' display='flex' flexDirection={['column','column']} >
             <Divider/>
-            <Box display='flex' h='100px' alignItems='center' justifyContent='space-between' w='full'>
+            <Box display='flex' h='100px' alignItems='center'flexDirection={['row','column']} justifyContent='space-between' w='full'>
                 <Image src='assets/zuri.png' alt=''/>
                 <Text color='#667085' fontWeight='400'>HNG Internship 9 Frontend Task</Text>
                 <Image alt='' src='assets/I4G.png'/>
