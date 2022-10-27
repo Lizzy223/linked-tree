44c6424cf97e8965ff27e5f1fe44e0a37e58218d
Dev-Waterways <113593306+Dev-Waterways@users.noreply.github.com>
2022-10-27 08:43:40 +0100

    made it mobile responsive

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -4,7 +4,7 @@ import {Box, Image, Button, Text, Divider} from '@chakra-ui/react'
 
 const Homepage = () => {
   return (
-    <Box w='100%' h='100%' display='flex' p={['1rem','4rem']} mt={['1.5rem','0']} justifyItems='center' alignItems='center' flexDirection='column'>
+    <Box w='full' h='100%' display='flex' p={['0rem','4rem']} mt={['1.5rem','0']} m='auto' justifyItems='center' alignItems='center' flexDirection='column'>
         <Box >
         <Image id='profile__img’' _hover={{src:'hover.png', cursor:'pointer'}} src='assets/img.png' w='80px' h='80px' alt='' />
         </Box>
@@ -19,31 +19,31 @@ const Homepage = () => {
         <Text fontWeight='700' display='none' visibility='hidden' id='slack' textAlign='center' mt='1rem' fontSize='20px'>
             Annette Black
         </Text>
-        <Box w='full' display='flex' flexDirection='column' gap='6' alignItems={['none','center']} justifyItems={['none','center']} h='100%' mt={['1rem','2rem']}>
-            <Button  as='a' href='https://twitter.com/hnginternship' target='_blank' w={['100%','90%']} h='4.75rem' m='auto' bg='#EAECF0' borderRadius='8px' color='#101828' fontWeight='500' fontSize='18px' border='1px solid #EAECF0'>
+        <Box w='full' display='flex' flexDirection='column' gap={['2','6']} alignItems={['none','center']} justifyItems={['none','center']} h='100%' mt={['1rem','2rem']}>
+            <Button  as='a' href='https://twitter.com/hnginternship' target='_blank' w={['90%','90%']} h='4.75rem' m='auto' bg='#EAECF0' borderRadius='8px' color='#101828' fontWeight='500' fontSize='18px' border='1px solid #EAECF0'>
                 Twitter link
             </Button>
-            <Button as='a' id='btn__zuri' href='https://training.zuri.team/' target='_blank' w={['100%','90%']} h='4.75rem' m='auto' bg='#EAECF0' borderRadius='8px' color='#101828' fontWeight='500' fontSize='18px' border='1px solid #EAECF0'>
+            <Button as='a' id='btn__zuri' href='https://training.zuri.team/' target='_blank' w={['90%','90%']} h='4.75rem' m='auto' bg='#EAECF0' borderRadius='8px' color='#101828' fontWeight='500' fontSize='18px' border='1px solid #EAECF0'>
                 Zuri Team
             </Button>
-            <Button as='a' id='books' href='https://books.zuri.team/' target='_blank' w={['100%','90%']} h='4.75rem' m='auto' bg='#EAECF0' borderRadius='8px' color='#101828' fontWeight='500' fontSize='18px' border='1px solid #EAECF0'>
+            <Button as='a' id='books' href='https://books.zuri.team/' target='_blank' w={['90%','90%']} h='4.75rem' m='auto' bg='#EAECF0' borderRadius='8px' color='#101828' fontWeight='500' fontSize='18px' border='1px solid #EAECF0'>
                 Zuri Books
             </Button>
-            <Button  as='a' id='book__python' href='https://books.zuri.team/python-for-beginners?ref_id=%3Cyourslackname%3E' target='_blank' w={['100%','90%']} h='4.75rem' m='auto' bg='#EAECF0' borderRadius='8px' color='#101828' fontWeight='500' fontSize='18px' border='1px solid #EAECF0'>
+            <Button  as='a' id='book__python' href='https://books.zuri.team/python-for-beginners?ref_id=%3Cyourslackname%3E' target='_blank' w={['90%','90%']} h='4.75rem' m='auto' bg='#EAECF0' borderRadius='8px' color='#101828' fontWeight='500' fontSize='18px' border='1px solid #EAECF0'>
                 Python Books
             </Button>
-            <Button  as='a' id='pitch' href='https://background.zuri.team/' target='_blank' w={['100%','90%']} h='4.75rem' m='auto' bg='#EAECF0' borderRadius='8px' color='#101828' fontWeight='500' fontSize='18px' border='1px solid #EAECF0'>
+            <Button  as='a' id='pitch' href='https://background.zuri.team/' target='_blank' w={['90%','90%']} h='4.75rem' m='auto' bg='#EAECF0' borderRadius='8px' color='#101828' fontWeight='500' fontSize='18px' border='1px solid #EAECF0'>
                 Background Check for Coders
             </Button>
-            <Button  as='a' id='book__design' href='https://books.zuri.team/design-rules' target='_blank' w={['100%','90%']} h='4.75rem' m='auto' bg='#EAECF0' borderRadius='8px' color='#101828' fontWeight='500' fontSize='18px' border='1px solid #EAECF0'>
+            <Button  as='a' id='book__design' href='https://books.zuri.team/design-rules' target='_blank' w={['90%','90%']} h='4.75rem' m='auto' bg='#EAECF0' borderRadius='8px' color='#101828' fontWeight='500' fontSize='18px' border='1px solid #EAECF0'>
                 Design books
             </Button>
-            <Box display={['none','flex']} padding='24px, 32px, 24px, 32px' mt={['0rem','1rem']} gap='6'>
+            <Box display={['flex','flex']} justifyContent='center' alignItems='center' padding={['.75rem','24px, 32px, 24px, 32px']} mt={['1rem','1rem']} gap={['3','6']}>
                 <Image src='assets/slack.png' alt=''/>
                 <Image alt='' src='assets/Icon.png'/>
             </Box>
         </Box>
-        <Box w='90%' mt='6rem' alignItems='center' display='flex' flexDirection={['column','column']} >
+        <Box w={['100%','90%']} mt={['3rem','6rem']} alignItems='center' display='flex' flexDirection={['column','column']} >
             <Divider/>
             <Box display='flex' h={['100%','100px']} alignItems='center' gap={['3','0']} flexDirection={['column','row']} justifyContent='space-between' w='full'>
                 <Image src='assets/zuri.png' alt=''/>
