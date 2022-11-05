7c30da35c681d2973e5215368abd25e37be97765
Kolade Elizabeth <koladeelizabeth01@gmail.com>
2022-11-05 11:13:47 +0100

    fix: bug fix contact page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -21,23 +21,23 @@ export default function Contact() {
             <HStack w='full' gap={['6',0]} display='flex' flexDirection={['column','row']} justifyContent='space-between'>
                 <VStack align='flex-start' w={['100%','50%']}>
                     <Text fontWeight='500' fontSize='14px' color='#344054'>First Name</Text>
-                    <Input placeholder='Enter your first name' fontWeight='400' fontSize='16px' borderRadius='8px' border='1px solid #D0D5DD' color='#667085' />
+                    <Input placeholder='Enter your first name' id='first_name’' fontWeight='400' fontSize='16px' borderRadius='8px' border='1px solid #D0D5DD' color='#667085' />
                 </VStack>
                  <VStack mt={['6','0']} align='flex-start' w={['100%','50%']}>
                     <Text fontWeight='500' fontSize='14px' color='#344054'>Last Name</Text>
-                    <Input placeholder='Enter your last name' fontWeight='400' fontSize='16px' borderRadius='8px' border='1px solid #D0D5DD' color='#667085' />
+                    <Input placeholder='Enter your last name' id='last_name’' fontWeight='400' fontSize='16px' borderRadius='8px' border='1px solid #D0D5DD' color='#667085' />
                 </VStack>
             </HStack>
             <HStack w='full' display='flex' flexDirection={['column','row']} justifyContent='space-between'>
                 <VStack align='flex-start' w={['100%','100%']}>
                     <Text fontWeight='500' fontSize='14px' color='#344054'>Email</Text>
-                    <Input placeholder='yourname@email.com' fontWeight='400' fontSize='16px' borderRadius='8px' border='1px solid #D0D5DD' color='#667085' />
+                    <Input placeholder='yourname@email.com' id='email' fontWeight='400' fontSize='16px' borderRadius='8px' border='1px solid #D0D5DD' color='#667085' />
                 </VStack>
             </HStack>
             <HStack w='full' display='flex' flexDirection={['column','row']} justifyContent='space-between'>
                 <VStack align='flex-start' w={['100%','100%']}>
                     <Text fontWeight='500' fontSize='14px' color='#344054'>Message</Text>
-                    <Textarea placeholder="Send me a message and I'll reply you as soon as possible..." fontWeight='400' fontSize='16px' borderRadius='8px' border='1px solid #D0D5DD' color='#667085' />
+                    <Textarea id='message' placeholder="Send me a message and I'll reply you as soon as possible..." fontWeight='400' fontSize='16px' borderRadius='8px' border='1px solid #D0D5DD' color='#667085' />
                 </VStack>
             </HStack>
            <HStack>
@@ -45,7 +45,7 @@ export default function Contact() {
              <Text fontSize='16px' fontWeight='400' color='#475467'>You agree to providing your data to name who may contact you.</Text>
            </HStack>
            
-            <Button bg='#1570EF' color='#fff' borderRadius='8px' border='1px solid #1570EF' w='100%' >Send Message </Button>
+            <Button bg='#1570EF' id='btn__submit' color='#fff' borderRadius='8px' border='1px solid #1570EF' w='100%' >Send Message </Button>
         </Box>
         <Box w={['100%','90%']} mt={['3rem','6rem']} alignItems='center' display='flex' flexDirection={['column','column']} >
             
