6030b42d6c53e3bcd61e03f02a3f0888b90d6584
Kolade Elizabeth <koladeelizabeth01@gmail.com>
2022-11-06 11:18:30 +0100

    fix: bug fix contact page

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -39,8 +39,8 @@ const Homepage = () => {
             <Button  as='a' id='book__design' href='https://books.zuri.team/design-rules' target='_blank' w={['90%','90%']} h='4.75rem' m='auto' bg='#EAECF0' borderRadius='8px' color='#101828' fontWeight='500' fontSize='18px' border='1px solid #EAECF0'>
                 Design books
             </Button>
-             <Button  id='contact'  w={['90%','90%']} h='4.75rem' m='auto' bg='#EAECF0' borderRadius='8px' color='#101828' fontWeight='500' fontSize='18px' border='1px solid #EAECF0'>
-               <Link to='/contact'> Contact Me</Link>
+             <Button  as='a' id='contact' href='https://linked-tree.vercel.app/contact' target='_blank' w={['90%','90%']} h='4.75rem' m='auto' bg='#EAECF0' borderRadius='8px' color='#101828' fontWeight='500' fontSize='18px' border='1px solid #EAECF0'>
+                Contact Me
             </Button>
             <Box display={['flex','flex']} justifyContent='center' alignItems='center' padding={['.75rem','24px, 32px, 24px, 32px']} mt={['1rem','1rem']} gap={['3','6']}>
                 <Image src='assets/slack.png' alt=''/>
